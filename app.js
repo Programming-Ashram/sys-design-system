6f63065511d25d47be1f08fa7b85db9b88f17f54
programmingashram <avenuedesign7658@gmail.com>
2023-03-18 18:18:39 +0530

    added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-collapse sidebar 
+//collapse sidebar 
 let sideBar = document.querySelector('.sys-sidebar-menu');
 collapseSidebar.addEventListener('click', function(){
     sideBar.classList.toggle('sidebar-collapse');
