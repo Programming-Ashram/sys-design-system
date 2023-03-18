462fbaf60d56301306066d566042515e70287e38
programmingashram <avenuedesign7658@gmail.com>
2023-03-18 17:22:46 +0530

    added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // collapse sidebar 
-let sideBar = document.querySelector('.sys-sidebar-menu');
-collapseSidebar.addEventListener('click', function(){
-    sideBar.classList.toggle('sidebar-collapse');
-})
+// let sideBar = document.querySelector('.sys-sidebar-menu');
+// collapseSidebar.addEventListener('click', function(){
+//     sideBar.classList.toggle('sidebar-collapse');
+// })
 
 
 
@@ -18,3 +18,34 @@ function infofunc(e, info) {
     document.getElementById(info).style.display = "block";
    
  }
+
+let snackbar = document.getElementsByClassName('sys-snackbar');
+let snackbarBtn = document.getElementsByClassName('snackbar-close-btn');
+for(let i = 0; i < snackbarBtn.length; i++){
+    snackbarBtn[i].addEventListener('click', function(){
+        this.parentElement.style.display = "none";
+    })
+}
+
+let snackbarTime1 = document.querySelectorAll('[time="5"]');
+let snackbarTime2 = document.querySelectorAll('[time="2"]');
+
+for(let i = 0; i < snackbarTime1.length; i++){
+    setTimeout(()=>{
+        if(snackbarTime1[i].style.display === "block"){
+            snackbarTime1[i].style.display = "block";
+        }else{
+            snackbarTime1[i].style.display = "none";
+        }
+    }, 5000);;
+}
+
+for(let i = 0; i < snackbarTime2.length; i++){
+    setTimeout(()=>{
+        if(snackbarTime2[i].style.display === "block"){
+            snackbarTime2[i].style.display = "block";
+        }else{
+            snackbarTime2[i].style.display = "none";
+        }
+    }, 2000);;
+}
