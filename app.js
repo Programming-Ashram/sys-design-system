b4458ed7984052922f0152c5a99efcc5d6c2cce2
programmingashram <avenuedesign7658@gmail.com>
2023-03-18 18:13:28 +0530

    add

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// collapse sidebar 
-// let sideBar = document.querySelector('.sys-sidebar-menu');
-// collapseSidebar.addEventListener('click', function(){
-//     sideBar.classList.toggle('sidebar-collapse');
-// })
+collapse sidebar 
+let sideBar = document.querySelector('.sys-sidebar-menu');
+collapseSidebar.addEventListener('click', function(){
+    sideBar.classList.toggle('sidebar-collapse');
+})
 
 
 
